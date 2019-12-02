750bf8fa47f85866b82689b0ddd01b9e8b7bae4d
Muhannad <55150185+MMSR-X9@users.noreply.github.com>
2019-12-02 20:54:09 +0400

    Update cart_info.php

--- cart_info.php
+++ cart_info.php
@@ -1,4 +1,35 @@
 <?php session_start();
+	if( isset($_GET['COMPON_DATA'])){
+			// query the user for the component page 
+
+				// see how many unique values in tage
+				$connection = mysqli_connect("localhost","root","","pc") or die("Error " . mysqli_error($connection));
+				$sql = "select tag from product group by tag;";
+				$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+				$row = mysqli_fetch_all($result);	
+				$total_tags = count($row);
+
+				$compo_data = [];
+
+				for($i = 0; $i != $total_tags; $i++){ 
+					$sql2 = "select * from product where tag = '".$row[$i][0]."' limit 3;";
+					$result2 = mysqli_query($connection, $sql2) or die("Error in Selecting " . mysqli_error($connection));
+					$row2 = mysqli_fetch_all($result2);
+					foreach($row2 as $prod){
+						if( strlen($prod[1]) > 25 ){
+							$prod[1] = substr($prod[1],0,25);	
+							$prod[1][22] = '.'; $prod[1][23] = '.'; $prod[1][24] = '.';
+						}
+						if( strlen($prod[4]) > 75 ){
+							$prod[4] = substr($prod[4],0,75);	
+							$prod[4][72] = '.'; $prod[4][73] = '.'; $prod[4][74] = '.';
+						}
+						array_push($compo_data,$prod);
+					}
+				}
+				echo json_encode($compo_data);
+	
+	}
 	if( isset($_POST['DEL_PROD']) ){
 		$key = array_search($_POST['DEL_PROD'], $_SESSION['cart']);
 		if ( $key !== false) {
@@ -25,7 +56,6 @@
 
 			$connection = mysqli_connect("localhost","root","","pc") or die("Error " . mysqli_error($connection));
 			 
-			
 			$pname;
 			$sql;
 			$pname = $_GET['PRODUCT_NAME'];
