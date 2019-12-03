720f1c1ebeab426b5267b48d9d5557f8f31f545e
Muhannad <55150185+MMSR-X9@users.noreply.github.com>
2019-12-03 04:51:30 +0400

    Update cart_session.php

--- cart_session.php
+++ cart_session.php
@@ -8,6 +8,31 @@
 				echo json_encode(array_values($_SESSION['cart'] = array_unique($_POST['CART_INFO'])));
 		}
 	}
+	else if ( isset($_GET['INDEX_DATA']) ) {
+		$connection = mysqli_connect("localhost","root","","pc") or die("Error " . mysqli_error($connection));
+		$sql = "select count(*) from product;";
+		$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+		$n_products = mysqli_fetch_row($result)[0];
+		$upper = rand(8, $n_products);
+		$lower = $upper - 7;
+		$data = [];
+			for(;$lower != $upper; $lower++){
+				$sql = "select * from product where pid = $lower;";
+				$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+				$row = mysqli_fetch_row($result);
+				
+				if( strlen($row[1]) > 25 ){
+							$row[1] = substr($row[1],0,25);	
+							$row[1][22] = '.'; $row[1][23] = '.'; $row[1][24] = '.';
+						}
+				if( strlen($row[4]) > 74 ){
+							$row[4] = substr($row[4],0,74);	
+							$row[4][71] = '.'; $row[4][72] = '.'; $row[4][73] = '.';
+					}
+					array_push($data,$row);
+			}
+			echo json_encode($data);
+	}
 	else if (isset($_POST['ROWS'])) {
 		include 'dbconnection.php';
 		$rows = [];
